86b1febac9d0c591cb31827e34c7292d92554e03
Coursera Learner <nobody@nowhere.nul>
2023-10-10 06:42:06 +0000

    did prj1

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 //I've provided "min" and "max" functions in
 //case they are useful to you
+
+typedef struct{
+	int x;
+	int y;
+	int height;
+	int width;
+}rectangle;
+rectangle intersection(rectangle r1, rectangle r2); 
+rectangle find_x_max(rectangle r1, rectangle r2);
+rectangle find_x_min(rectangle r1, rectangle r2);
+
+rectangle find_y_min(rectangle r1, rectangle r2);
+rectangle find_y_max(rectangle r1, rectangle r2);
 int min (int a, int b) {
   if (a < b) {
     return a;
@@ -14,17 +27,87 @@ int max (int a, int b) {
   }
   return b;
 }
+rectangle find_x_max(rectangle r1, rectangle r2){
+	if (r1.x>r2.x){
+		return r1;
+	}
+	else{
+		return r2;
+	}
+rectangle find_x_min(rectangle r1, rectangle r2){
+	rectangle max=find_x_max(r1,r2);
+	if(max.x==r1.x&&max.y==r1.y&&max.height==r1.height&&max.width==r1.width){
+		return r2;
+	}
+	else{
+		return r1;
+	}
 
+rectangle find_y_max(rectangle r1, rectangle r2){
+	if (r1.y>r2.y){
+		return r1;
+	}
+	else{
+		return r2;
+	}
+rectangle find_y_min(rectangle r1, rectangle r2){
+	rectangle max=find_y_max(r1,r2);
+	if(max.x==r1.x&&max.y==r1.y&&max.height==r1.height&&max.width==r1.width){
+		return r2;
+	}
+	else{
+		return r1;
+	}
 //Declare your rectangle structure here!
 
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+	if(r.height<0){
+		r.height=-r.height;
+		r.y=r.y-r.height;
+	}
+	if(r.width<0){
+		r.width=-r.width;
+		r.x=r.x-r.width;
+	}
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
-  return r1;
+	rectangle x_max=find_x_max(r1,r2);
+	rectangle x_min=find_x_min(r1,r2);
+	rectangle y_max=find_y_max(r1,r2);
+	rectangle y_min=find_y_min(r1,r2);
+	//judge if intersection
+	if (x_max.x<x_min.x+x_min.w && 
+			y_max.y<y_min.y+y_min.h){
+		//distinguish x
+		if (x_max.x+x_max.w>x_min.x+x_min.w){
+			//distinguish y
+			if (y_max.y+y_max.h>y_min.y+y_min.h){
+			rectangle anw1={x_max.x,y_max.y,y_min.height-y_max.y+y_min.y, x_min.width-x_max.x+x_min.x};
+				return anw1;
+			}
+			else{
+				rectangle anw2={x_max.x,y_max.y,y_max.height, x_min.width-x_max.x+x_min.x}
+			}
+		}
+		else{
+			if (y_max.y+y_max.h>y_min.y+y_min.h){
+				rectangle anw3={x_max.x,y_max.y, y_min.height-y_max.y+y_min.y, x_max.width};
+				return anw3;
+			}
+			else{
+				rectangle anw4={x_max.x ,y_max.y, y_max.height, x_max.width};
+				return anw4;
+			}
+		}
+	}
+	else{
+		rectangle zero={0,0,0,0};
+		return zero;
+	}
 }
 
 //You should not need to modify any code below this line
